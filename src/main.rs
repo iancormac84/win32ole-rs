1c7e5e3b15414efa38386f49010909e5ab01217f
iancormac84 <wilnathan@gmail.com>
2023-01-09 19:17:10 -0400

    Going back to main to bug hunt.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let excel= win32ole::OleTypeData::new("Microsoft Excel 16.0 Object Library", "Workbook").unwrap();
     let ole_types = excel.implemented_ole_types()?;
+    println!("ole_types length is {}", ole_types.len());
     for ole_type in ole_types {
         println!("{}", ole_type.name());
         let methods = ole_type.ole_methods()?;
+        println!("methods len is {}", methods.len());
         for method in methods {
             println!("\t{}", method.name());
             let params = method.params();
+            println!("params len is {}", params.len());
             for param in params {
                 let param = param?;
                 println!("\t\t{}", param.name());
             }
         }
         let variables = ole_type.variables()?;
+        println!("variables len is {}", variables.len());
         for variable in variables {
             println!("\tVAR: {}", variable.name());
         }
