9f6a227b0fe0aafea541d31894bf96eee7a85242
iancormac84 <wilnathan@gmail.com>
2023-01-11 18:19:12 -0400

    This seems to work without crashing.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let excel =
-        win32ole::OleTypeData::new("Microsoft Excel 16.0 Object Library", "Workbook").unwrap();
+    let excel = win32ole::OleTypeData::new("Microsoft Excel 16.0 Object Library", "Worksheet")?;
+    let ole_types = excel.implemented_ole_types()?;
     let methods = excel.ole_methods()?;
-    println!("methods length is {}", methods.len());
-    /*for ole_type in ole_types {
+    println!("methods.len() is {}", methods.len());
+    for method in methods {
+        println!("    {}", method.name());
+    }
+    for ole_type in ole_types {
         println!("{}", ole_type.name());
-        let methods = ole_type.ole_methods()?;
-        println!("methods len is {}", methods.len());*/
-        for method in methods {
-            println!("\t{}", method.name());
-            /*let params = method.params();
-            println!("params len is {}", params.len());
-            for param in params {
-                let param = param?;
-                println!("\t\t{}", param.name());
-            }*/
+        let ole_type_methods = ole_type.ole_methods()?;
+        println!("ole_type_methods.len() is {}", ole_type_methods.len());
+        for ole_type_method in ole_type_methods {
+            println!("    {}", ole_type_method.name());
         }
-        /*let variables = ole_type.variables()?;
-        println!("variables len is {}", variables.len());
-        for variable in variables {
-            println!("\tVAR: {}", variable.name());
-        }
-    }*/
+    }
     Ok(())
 }
