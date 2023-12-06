08cb5f156a60b635806c3cbec057dc2592c7e678
iancormac84 <wilnathan@gmail.com>
2023-12-06 18:36:33 -0400

    Updated to use the winreg crate for registry access.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let excel = win32ole::OleData::new("Excel.Application")?;
+    let excel = win32ole::OleData::new("Word.Application")?;
     let excel_tlib = excel.ole_typelib()?;
     let path = excel_tlib.path()?;
     println!("{}", path.display());
